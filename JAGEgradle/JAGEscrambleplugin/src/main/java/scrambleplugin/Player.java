16084247800ef04bcfa250b93df8ef45d68253da
valarion <valarionch@gmail.com>
2015-12-22 20:11:44 +0100

    Cleaned warnings and organized imports

--- JAGEgradle/JAGEscrambleplugin/src/main/java/scrambleplugin/Player.java
+++ JAGEgradle/JAGEscrambleplugin/src/main/java/scrambleplugin/Player.java
@@ -34,7 +34,6 @@ import org.newdawn.slick.SlickException;
 import org.w3c.dom.Element;
 
 import com.valarion.gameengine.core.Event;
-import com.valarion.gameengine.core.GameCore;
 import com.valarion.gameengine.core.tiled.SubTiledMap;
 import com.valarion.gameengine.gamestates.Controls;
 import com.valarion.gameengine.gamestates.Database;
